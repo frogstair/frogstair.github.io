2e114887595cc4dcdc5a067dde7153666c8742a2
frogstair <daniladudkin412@gmail.com>
2021-01-26 14:05:57 +0200

    Add projects

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(() => {
         setLight();
         $("#darkbox").prop("checked", false);
     }
+
     $("body").show();
 })
 
@@ -25,4 +26,17 @@ function setDark() {
     Cookies.set("dark", "1");
     $(".darkmode-slider::before").addClass("dark")
     $(".darkable").addClass("dark")
+}
+
+function generateEmail() {
+
+    var emails = [
+        "Begin your essay here...",
+        "Hello Daniel!",
+        "Whats up?",
+        "Begin your formal email:"
+    ]
+
+    var email = encodeURIComponent(emails[Math.floor(Math.random() * emails.length)]);
+    $("#email").attr("href", `mailto:daniladudkin412@gmail.com?subject=Hey!&body=${email}`)
 }
